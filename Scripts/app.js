14f301585466ad81803c8e684247e97c300da057
blanegraff <brittany@Brittanys-MacBook-Pro.local>
2016-02-04 11:46:44 -0500

    Lesson-4
    Updated app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,22 @@
 "use strick";
 
 // setup your IIFE(Immediately Invoked Function Expression)
-(function(){
-      
+(function () {
+
       console.log("App Started...");
       
+      //declared a named function way
+      function replaceFirstParagraph() {
+            console.log("inside replaceFirstParagraph function");
+            var firstParagraph;
+
+            firstParagraph = document.getElementById("firstParagraph");
+
+            firstParagraph.innerHTML = "My New Paragraph Data";
+      }
       
+      //call the replace first paragraph function
+      replaceFirstParagraph();
+
+
 })();
