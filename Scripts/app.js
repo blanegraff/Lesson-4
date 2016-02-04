9ac93749aa5d06ebcd2e9d1e37f95d4bb25cf06f
blanegraff <brittany@Brittanys-MacBook-Pro.local>
2016-02-04 13:02:46 -0500

    Lesson-4

--- Scripts/app.js
+++ Scripts/app.js
@@ -6,6 +6,7 @@
       console.log("App Started...");
       
       //declared a named function way
+/*
       function replaceFirstParagraph() {
             console.log("inside replaceFirstParagraph function");
             var firstParagraph;
@@ -14,6 +15,16 @@
 
             firstParagraph.innerHTML = "My New Paragraph Data";
       }
+*/
+      // Declasre an anonymous funtion with named alias *NO NO*
+      var replaceFirstParagraph = function() {
+            console.log("inside replaceFirstParagraph anonymous funtion");
+            var firstParagraph;
+            
+            firstParagraph = Document.getElementById("firstParagraph");
+            
+            firstParagraph.innerHTML = "My New paragraph data";
+      };
       
       //call the replace first paragraph function
       replaceFirstParagraph();
